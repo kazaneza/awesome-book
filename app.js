c3376b547c92a0042e8c7cc2efa37cf88ed5c81e
kazaneza <kazanezagentil@gmail.com>
2023-01-16 15:01:56 +0100

    add, remove, update and display books in js

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-use-before-define */
-
-class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
+ // Getting values from input fields
+ const form = document.querySelector('form');
+ form.addEventListener('submit', (e) => {
+   const title = document.querySelector('.title');
+   const author = document.querySelector('.author');
+   e.preventDefault();
+   addBook(title.value, author.value);
+ });
+ 
+  // store book 
+  const storeData = JSON.parse(localStorage.getItem('Added Books'));
+  
+  function updateBook() {
+    localStorage.setItem('Added Books', JSON.stringify(storeData));
   }
   
+  function createBooks(arr) {
+    let books = '';
+    for (let i = 0; i < arr.length; i += 1) {
+      books += `
+              <p>${arr[i].title}</p>
+              <p>${arr[i].author}</p>
+              <button onclick="removeBook(${i})">Remove</button>
+              <hr/>
+              `;
+    }
+    return books;
+  }
+
+  // Diplaying books 
+  function DispayAwesomeBooks() {
+    const listOfBooks = document.querySelector('.books-list');
+    listOfBooks.innerHTML = `
+                <ul class="book-list"/>
+                ${createBooks(storeData)}</ul>
+            `;
+  }
+  
+  // Adding books
+  function addBook(bookTitle, bookAuthor) {
+    const Book = {
+      title: bookTitle,
+      author: bookAuthor,
+    };
+    storeData.push(Book);
+    updateBook();
+    DispayAwesomeBooks();
+  }
+  // Remove data from local storage
+  function removeBook(i) {
+    storeData.splice(i, 1);
+    updateBook();
+    DispayAwesomeBooks();
+  }
+  removeBook();
+  
  
+  
+  window.onload = DispayAwesomeBooks();
