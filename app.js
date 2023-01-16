db86711082954eff530214562244d2db978eee30
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-01-16 14:08:23 +0200

    create UT

---
+++ app.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-use-before-define */
+
+class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
+ 
